f263a81731883b2bcc9dda14687c87c7eb48fe4f
Kandiyah <ijegwajemimah@gmail.com>
2023-02-21 15:21:28 -0800

    Jack Bauer

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,13 +5,16 @@
  *
  * @c: parameter to be checked
  *
- * return: Always 0
+ * return: Always 0.
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
