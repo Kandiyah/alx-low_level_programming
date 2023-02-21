6cf94b43dabdaeeea47c4c84a5fac5d628f30db1
Kandiyah <ijegwajemimah@gmail.com>
2023-02-21 15:45:23 -0800

    the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * print_to_98 - count from n to 98.
+ *@n:integer.
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	printf("98\n");
+}
