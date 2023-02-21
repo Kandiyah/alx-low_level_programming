de0a3d9fbefa91149203868301cb2459ec1712c7
Kandiyah <ijegwajemimah@gmail.com>
2023-02-21 12:28:08 -0800

    readme file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints signs
+ *
+ * @n: function parameter
+ *
+ * return: 1, 0 and -1
+ */
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}	
+}
