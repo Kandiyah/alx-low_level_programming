154f57005895a670b2c104f01c15da55c462eeb5
Kandiyah <ijegwajemimah@gmail.com>
2023-02-21 16:25:56 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - function that prints multiple numbers.
+ *
+ * Return:void
+ */
+int main(void)
+{
+	int a, b, s, x, y;
+
+	s = 0;
+	for (a = 0; a <= (1024  / 3); a++)
+	{
+		y = a * 3;
+		s = s + y;
+	}
+	for (b = 0; b < (1024 / 5); b++)
+	{
+		if (!(b % 3 == 0))
+		{
+			x = b * 5;
+			s = s + x;
+		}
+	}
+	printf("%i\n", s);
+
+	return (0);
+}
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * _isalpha - function that checks for alphabets
- *
- * @c: parameter to be checked
- *
- * return: Always 0.
+ * _isalpha - check alphabetic charachter.
+ * @c: charachter
+ * Return: Always 0.
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
 	{
 		return (1);
 	}
